a399c71cbf43fe942af2e227e26266f96dcdb608
Ken Sasongko <ken.sasongko@gmail.com>
2022-07-21 09:29:07 +0700

    feat: add pages and API call to API Gateway

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,56 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+import axios from "axios"
+import { useEffect, useState } from 'react'
+import { Input } from '@chakra-ui/react'
+import { Center, Flex, Box, Text } from '@chakra-ui/react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+const apiUrl = `${apiBaseUrl}/todos`;
+axios.defaults.headers.common = {'X-Api-Key': apiKey};
+
+const fetcher = url => axios.get(url).then(res => res.data);
 
 export default function Home() {
+
+  //const poster = (data: any) => async (url: string) => await axios({method: 'post', url, data }).then((res) => res.data);
+  const { data, error } = useSWR(apiUrl, fetcher);
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(()=> {
+    if(data){
+      setTodos(data.Items);
+    }
+  }, [data])
+
+  const handleSave = async (event) => {
+    await axios.post(apiUrl, { 
+      title: title,
+      message: message,
+    })
+    const { Items } = await fetcher(apiUrl);
+    if(Items){
+      setTodos(Items);
+    }
+    setTitle('');
+    setMessage('');
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +59,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+      <Box><Text fontSize="4xl">My Simple TODO Application</Text></Box>
+      <Flex w={'100%'}>
+        <Box flex='1'>
+          <Accordion>
+            {
+              todos.map((row, index) => {
+                return (
+                  <AccordionItem key={row.id}>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex='1' textAlign='left'>Title: {row.title}</Box><AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      {row.message}
+                    </AccordionPanel>
+                  </AccordionItem>
+                )
+              })
+            }
+          </Accordion>
+        </Box>
+        <Center w='400px'>
+          <Box m={2} p={2} borderWidth='1px' borderRadius='lg'>
+            <Box my={2} mx={1}><Text>Create TODO entry</Text></Box>
+            <Box my={2} mx={1}><Input placeholder='Title' value={title} onChange={(event) => { setTitle(event.target.value) }}/></Box>
+            <Box my={2} mx={1}><Input placeholder='Message' value={message} onChange={(event) => { setMessage(event.target.value) }}/></Box>
+            <Box my={2} mx={1}><Button colorScheme='teal' onClick={handleSave}>Save</Button></Box>
+          </Box>
+        </Center>
+      </Flex>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
